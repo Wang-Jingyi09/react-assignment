0983a1268a27330c07483a6221caeaba6aa8a73e
Jingyi Wang <20104728@email.wit.ie>
2023-11-20 22:13:07 +0000

    Initial commit.

---
+++ movies/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from '../components/spinner';
+import AddToMustWatchIcon from '../components/cardIcons/addToMustWatch'; 
+
+const UpcomingMoviesPage = () => {
+    const {data, error, isLoading, isError }  = useQuery("upcoming", getUpcomingMovies);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    if (isError) {
+        return <h1>{`Error: ${error.message}`}</h1>;
+    }
+    
+    const movies = data.results;
+    const mustWatch = movies.filter(m => m.mustWatch)
+
+    
+    localStorage.setItem('favorites', JSON.stringify(mustWatch))
+
+
+
+
+    return (
+        <PageTemplate
+            title="Upcoming Movies"
+            movies={movies}
+            action={movie => {
+                return <AddToMustWatchIcon movie={ movie }/>
+            }}
+        />
+    );
+};
+
+export default UpcomingMoviesPage;
